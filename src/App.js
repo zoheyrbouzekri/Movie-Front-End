ded4f9fee7c8f06a8d411064427ca740f01eca63
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-08 18:31:31 +0100

    Router Last Version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Favorite from './pages/Favorite';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorite' element={<Favorite />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -1,7 +1,8 @@
 const Favorite = () => {
     return (
-        <div className="favorite">
-
+        <div className="favorite-empty">
+            <h2>No Favorite Movies Yet</h2>
+            <p>Start Adding Favorite Movies</p>
         </div>
     );
 }
