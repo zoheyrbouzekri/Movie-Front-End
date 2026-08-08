936794878e12dc561637f66fadc5f9d33f5b7750
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-08 14:00:09 +0100

    Movie Card Component

---
+++ src/components/MovieCard.js
@@ -0,0 +1,25 @@
+const MovieCard = ({movie}) => {
+
+    const hundleFavoriteClick = () => {
+        alert("Clicked")
+    }
+
+    return ( 
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title}/>
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={hundleFavoriteClick}>
+                        ♥
+                    </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
