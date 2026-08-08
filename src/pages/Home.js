3b15d6850b1d883a21879e413520182a32476643
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-08 18:12:57 +0100

    Search Query

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard";
 
 const Home = () => {
 
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies=[
         {id:1, title:"La vie Scholaire" , release_date:"2019"},
-        {id:1, title:"CREED" , release_date:"2020"},
-        {id:1, title:"CREED II" , release_date:"2021"},
-        {id:1, title:"CARS" , release_date:"2016"}    ]
+        {id:2, title:"CREED" , release_date:"2020"},
+        {id:3, title:"CREED II" , release_date:"2021"},
+        {id:4, title:"CARS" , release_date:"2016"}    ]
+
+    const hundleSeach = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery("")
+    }
 
     return ( 
         <div className="home">
+            <form onSubmit={hundleSeach} className="search-form">
+            <input type="text" placeholder="Search a film here!" className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+                <button type="submit" className="search-button">Search</button>
+            </form>
             {movies.map((movie) => (
-                <MovieCard movie={movie} key={movie.id}/>
+                movie.title.toLowerCase().startsWith(searchQuery.toLocaleLowerCase()) && (
+                <MovieCard movie={movie} key={movie.id}/>)
             ))}
         </div>
     );
